2b733da3ee992ba4da9c7691ebb326ccaa0bc48f
Vasu <vasusojitra167@gmail.com>
2022-03-12 15:45:16 +0530

    [ADD] Ecommerce App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, {useState} from "react";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./Components/Dashboard";
+import ItemPreview from "./Components/ItemPreview";
 import Counter from "./Counter";
 
 const App = () => {
   return <React.Fragment>
-    <Counter />
+    <Routes>
+      <Route path="/" element={<Dashboard />}></Route>
+      <Route path="/:id" element={<ItemPreview />}></Route>
+    </Routes>
   </React.Fragment>;
 };
 
